4f2197ac6b6d2e3cb72c7719d5ed7174a4cb17cf
Dmytro <dmytro.shklianka.upwork@gmail.com>
2022-02-18 19:59:53 +0200

    Add some styles

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -9,15 +9,22 @@ import { login } from '../services';
 import { loginSuccess } from '../redux/slices/auth';
 import { useHistory } from "react-router-dom";
 import { userToken } from '../types/index';
+import Cylbellum from "./../imges/react-test-wrong-credentials 1.png"
 
 const useStyles = makeStyles({
+  container: {
+    display:'flex',
+    flexDirection: "row",
+  },
+  img: {
+    height: '100vh'
+  },
   root: {
     display: 'flex',
     flexDirection: 'column',
-    padding: '10% 30%',
   },
   mainInfo: {
-    marginBottom: 50,
+    margin: '100px 0px 50px 50px',
   },
   h1: {
     fontSie: 26,
@@ -25,7 +32,7 @@ const useStyles = makeStyles({
     color: '#53616A',
   },
   span: {
-    fontSie: 26,
+    fontSize: 15,
     opacity: '50%',
     color: '#53616A',
   },
@@ -33,17 +40,24 @@ const useStyles = makeStyles({
     display: 'flex',
     flexDirection: 'column',
     gap: 20,
+    width: 540,
+    margin: '0px 0px 50px 50px',
   },
   label: {
     color: '#707886',
-    fontSize: 13,
+    fontSize: 16,
   },
   inputType: {
+    boxSizing:"border-box",
     backgroundColor: '#f3f5f7',
     outline: 'none',
     borderRadius: 3,
     fontSize: 16,
     color: ' #000',
+    width: 370,
+    "& input": {
+      padding: "9px 14px",
+    }
   },
   formButton: {
     backgroundColor: '#5e8751',
@@ -55,7 +69,7 @@ const useStyles = makeStyles({
   errorMessage: {
     color: 'red',
     fontSize: 13, 
-  }
+  },
 });
 
 export default function LoginPage() {
@@ -67,7 +81,7 @@ export default function LoginPage() {
   const [emailIsEmpty,setEmailIsEmpty] = useState<boolean>(false);
   const [passwordIsEmpty, setPasswordIsEmpty] = useState<boolean>(false);
 
-  const borderEmeail = emailIsEmpty ? '1px solid red' : '1px solid #000';
+  const borderEmail = emailIsEmpty ? '1px solid red' : '1px solid #000';
   const borderPassword = passwordIsEmpty ? '1px solid red' : '1px solid #000';
   const [badCredentials, setBadCredentials] = useState<boolean>(false);
   const history = useHistory();
@@ -106,57 +120,76 @@ export default function LoginPage() {
     })
 
   };
+
   return (
-    <Box className={classes.root}>
-      <Box className={classes.mainInfo}>
-        <h1 className={classes.h1}>Log in</h1>
-        <span className={classes.span}>
-          Enter your Email and Password. If you don't have an account, plesase,
-          see your email invintational{' '}
-        </span>
-      </Box>
       <Box
-        className={classes.form}
-        component="form"
-        noValidate
-        autoComplete="off"
-        onSubmit={(e: any) => formHandler(e)}
+          className={classes.container}
       >
-        <label className={classes.label} htmlFor="email">
-          Email
-        </label>
-        <TextField
-          value={emailValue}
-          onChange={(e) => dispatch(setEmail(e.target.value))}
-          className={classes.inputType}
-          style={{border: `${borderEmeail}`}}
-          id="email"
-          variant="outlined"
-        />
-        {!badCredentials && emailIsEmpty && <span className={classes.errorMessage}>Email required</span>}
-        <label className={classes.label} htmlFor="passwords">
-          Password
-        </label>
-        <TextField
-          value={passwordValue}
-          onChange={(e) => dispatch(setPassword(e.target.value))}
-          type="password"
-          className={classes.inputType}
-          sx={{border: `${borderPassword}`}}
-          id="passwords"
-          variant="outlined"
-        />
-         {!badCredentials && passwordIsEmpty && <span className={classes.errorMessage}>Password required</span>}
-         {badCredentials && <span className={classes.errorMessage}>Wrong Credentials</span>}
-        <Button
-          sx={{ width: '300px', marginLeft: '470px', marginTop: '50px' }}
-          size="large"
-          type="submit"
-          variant="contained"
-        >
-          Login
-        </Button>
+        <Box>
+          <img
+              className={classes.img}
+              src={Cylbellum}
+              alt="Cylbellum-img"
+              loading="lazy"
+          />
+        </Box>
+      <Box className={classes.root}>
+          <Box className={classes.mainInfo}>
+            <h1 className={classes.h1}>Log in</h1>
+            <span className={classes.span}>
+              Enter your Email and Password. If you don't have
+              <br/>
+              an account, please,
+              see your email invitation{' '}
+            </span>
+          </Box>
+          <Box
+            className={classes.form}
+            component="form"
+            noValidate
+            autoComplete="off"
+            onSubmit={(e: any) => formHandler(e)}
+          >
+            <label className={classes.label} htmlFor="email">
+              Email
+            </label>
+            <TextField
+              value={emailValue}
+              onChange={(e) => dispatch(setEmail(e.target.value))}
+              className={classes.inputType}
+              sx={{border: `${borderEmail}`}}
+              id="email"
+              variant="outlined"
+            />
+            {!badCredentials && emailIsEmpty && <span className={classes.errorMessage}>Email required</span>}
+            <label className={classes.label} htmlFor="passwords">
+              Password
+            </label>
+            <TextField
+              value={passwordValue}
+              onChange={(e) => dispatch(setPassword(e.target.value))}
+              type="password"
+              className={classes.inputType}
+              sx={{border: `${borderPassword}`}}
+              id="passwords"
+              variant="outlined"
+            />
+             {!badCredentials && passwordIsEmpty && <span className={classes.errorMessage}>Password required</span>}
+             {badCredentials && <span className={classes.errorMessage}>Wrong Credentials</span>}
+            <Button
+              sx={{
+                width: '200px',
+                marginLeft: '170px',
+                marginTop: '40px'
+              }}
+              size="large"
+              type="submit"
+              variant="contained"
+            >
+              Login
+            </Button>
+          </Box>
+        </Box>
       </Box>
-    </Box>
   );
 }
